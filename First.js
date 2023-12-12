2c59e87d7cc5934d81e92cc151b0d908e3441591
RDX-Rudra <dasrudra234@gmail.com>
2023-12-12 18:12:08 +0530

    Javascript

---
+++ First.js
@@ -0,0 +1,34 @@
+console.log("Rudra Das");
+console.log('Welcome Rudra Das');
+age = 24;
+console.log(age);
+full_name= "rudra";
+console.log(full_name);
+a = 55.23;
+b = null;
+c= undefined;
+console.log(a, b, c);
+t = true;
+console.log(t);
+t = 21;
+console.log(t);
+f = true;
+a = f;
+console.log(f);
+console.log(a);
+$a = 51;
+console.log($a);
+let r;  //cannot be re decleared but can update
+console.log(r); //Undefined
+r = 15; //cannot be re decleared but can update
+console.log(r);
+const car = "Farrari"; // constant variable with value
+console.log(car);
+{
+    let a = 10;
+    console.log(a);
+}
+{
+    let a = 25;
+    console.log(a);
+}
