08d4e10d22f42f2e6ab9d02875e89824e84036f6
RDX-Rudra <dasrudra234@gmail.com>
2024-03-10 01:33:38 +0530

    js

---
+++ objectAndConstructor/objectConstructor.js
@@ -0,0 +1,11 @@
+function Player(name, marker){
+    this.name = name;
+    this.marker = marker;
+}
+
+const player = new Player ('rudra', 'x');
+console.log(player.name);
+
+function Playerx (name, marker){
+    this
+}
