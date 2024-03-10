526a83d86e7f8126047a1ff6321bda2e9c1e7c55
RDX-Rudra <dasrudra234@gmail.com>
2024-03-11 00:18:11 +0530

    js

--- objectAndConstructor/objectConstructor.js
+++ objectAndConstructor/objectConstructor.js
@@ -7,5 +7,14 @@ const player = new Player ('rudra', 'x');
 console.log(player.name);
 
 function Playerx (name, marker){
-    this
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function(){
+        console.log(this.name);
+    };
 }
+
+const player1 = new Playerx('ullwal', 'x');
+const player2 = new Playerx('Rdx', 'o');
+player1.sayName();
+player2.sayName();
