f59f5e1f2c6c2eb8385b1eb8b8d08465deefba29
RDX-Rudra <dasrudra234@gmail.com>
2024-03-11 01:33:56 +0530

    js

--- objectAndConstructor/bookExer.js
+++ objectAndConstructor/bookExer.js
@@ -1,17 +1,24 @@
-function Book(title, author, pages){
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     let read = false;
-    this.info = function(){
-        if(read  === true){
-            console.log("The " + this.title + " by " + this.author +" , "+ this.pages+" pages, not read yet");
-        }
-        else{
-            console.log("The " + this.title + " by " + this.author +" , "+ this.pages+" pages, read");
+
+    this.toggleRead = function() {
+        read = !read; // Toggle the read status
+    };
+
+    this.info = function() {
+        if (read === true) {
+            console.log("The " + this.title + " by " + this.author + ", " + this.pages + " pages, read.");
+        } else {
+            console.log("The " + this.title + " by " + this.author + ", " + this.pages + " pages, not read yet.");
         }
     };
 }
 
 const theHobbit = new Book('theHobbit', 'J.R.R. Tolkien', 295);
+theHobbit.info();
+theHobbit.pages = 300;
+theHobbit.toggleRead(); // Mark the book as read
 theHobbit.info();
--- objectAndConstructor/objectConstructor.js
+++ objectAndConstructor/objectConstructor.js
@@ -18,3 +18,12 @@ const player1 = new Playerx('ullwal', 'x');
 const player2 = new Playerx('Rdx', 'o');
 player1.sayName();
 player2.sayName();
+// #############################
+Object.getPrototypeOf(player1) === Player.prototype; // returns true
+Object.getPrototypeOf(player2) === Player.prototype; // returns true
+Player.prototype.sayHello = function() {
+   console.log("Hello, I'm a player!");
+};
+
+player1.sayHello(); // logs "Hello, I'm a player!"
+player2.sayHello(); // logs "Hello, I'm a player!"
