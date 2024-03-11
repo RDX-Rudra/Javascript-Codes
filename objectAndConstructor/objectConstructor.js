d70c3756ba4685da7812e6608ca746b5e95abb9c
RDX-Rudra <dasrudra234@gmail.com>
2024-03-11 18:16:37 +0530

    js

--- objectAndConstructor/objectConstructor.js
+++ objectAndConstructor/objectConstructor.js
@@ -19,11 +19,17 @@ const player2 = new Playerx('Rdx', 'o');
 player1.sayName();
 player2.sayName();
 // #############################
-Object.getPrototypeOf(player1) === Player.prototype; // returns true
-Object.getPrototypeOf(player2) === Player.prototype; // returns true
-Player.prototype.sayHello = function() {
+Object.getPrototypeOf(player1) === Playerx.prototype; // returns true
+Object.getPrototypeOf(player2) === Playerx.prototype; // returns true
+Playerx.prototype.sayHello = function() {
    console.log("Hello, I'm a player!");
 };
 
 player1.sayHello(); // logs "Hello, I'm a player!"
 player2.sayHello(); // logs "Hello, I'm a player!"
+
+// Player.prototype.__proto__
+Object.getPrototypeOf(Playerx.prototype) === Object.prototype; // true
+
+// Output may slightly differ based on the browser
+console.log(player1.valueOf()); // Output: Object { name: "steve", marker: "X", sayName: sayName() }
